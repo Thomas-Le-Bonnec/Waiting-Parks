be007e597ac8c388d40fb4b8a19d8f876987e10c
TheBonnec <thomaslebonnec@icloud.com>
2023-12-10 23:22:42 -0500

    finito (finito) le projet

--- src/data/parks.js
+++ src/data/parks.js
@@ -1,11 +1,11 @@
 class Park {
-    constructor(name, country, image, apiURL) {
+    constructor(name, country, image, apiURL, regularPrice, fastPassPrice) {
         this.name = name;
         this.country = country;
         this.image = image;
         this.apiURL = apiURL;
-        this.regalarPrice = 10;
-        this.fastPassPrice = 20;
+        this.regularPrice = regularPrice;
+        this.fastPassPrice = fastPassPrice;
     }
 }
 
@@ -21,15 +21,15 @@ class Country {
 
 
 // France
-const disneylandPark = new Park("Disneyland Park", "France", "https://source.unsplash.com/brown-and-blue-castle-under-cloudy-sky-during-daytime-Lmd-CpZOGWc", "/api/parks/4/queue_times.json");
-const waltDisneyStudio = new Park("Walt Disney Studio", "France", "https://source.unsplash.com/brown-concrete-building-under-white-sky-during-daytime-bqBy9Rqje6k", "/api/parks/28/queue_times.json");
-const parcAsterix = new Park("Parc Astérix", "France", "https://s3.travel-cdn.net/pictures/73538/raw.jpg?rev=3", "/api/fr/parks/9/queue_times.json");
-const futuroscope = new Park("Futuroscope", "France", "https://cdn.sortiraparis.com/images/80/101137/922095-futuroscope-visuels-juillet-2023.jpg", "/api/fr/parks/291/queue_times.json");
-const walibiRhoneAlpes = new Park("Walibi Rhône-Alpes", "France", "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/84/81/ba/mystic-nouvelle-attraction.jpg?w=1200&h=-1&s=1", "/api/fr/parks/301/queue_times.json");
+const disneylandPark = new Park("Disneyland Park", "France", "https://source.unsplash.com/brown-and-blue-castle-under-cloudy-sky-during-daytime-Lmd-CpZOGWc", "/api/parks/4/queue_times.json", 90, 180);
+const waltDisneyStudio = new Park("Walt Disney Studio", "France", "https://source.unsplash.com/brown-concrete-building-under-white-sky-during-daytime-bqBy9Rqje6k", "/api/parks/28/queue_times.json", 90, 180);
+const parcAsterix = new Park("Parc Astérix", "France", "https://s3.travel-cdn.net/pictures/73538/raw.jpg?rev=3", "/api/fr/parks/9/queue_times.json", 50, 120);
+const futuroscope = new Park("Futuroscope", "France", "https://cdn.sortiraparis.com/images/80/101137/922095-futuroscope-visuels-juillet-2023.jpg", "/api/fr/parks/291/queue_times.json", 40, 110);
+const walibiRhoneAlpes = new Park("Walibi Rhône-Alpes", "France", "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/84/81/ba/mystic-nouvelle-attraction.jpg?w=1200&h=-1&s=1", "/api/fr/parks/301/queue_times.json", 60, 120);
 
 // USA
-const disneylandLosAngeles = new Park("Disneyland Los Angeles", "USA", "https://source.unsplash.com/the-top-of-a-building-under-a-cloudy-blue-sky-45P-Jib0OHA", "/api/parks/16/queue_times.json");
-const disneyCaliforniaAdventure = new Park("Disneyland California Adventure", "USA", "https://source.unsplash.com/a-ferris-wheel-with-flowers-in-front-of-it-G_IW5FLKZiQ", "/api/parks/17/queue_times.json");
+const disneylandLosAngeles = new Park("Disneyland Los Angeles", "USA", "https://source.unsplash.com/the-top-of-a-building-under-a-cloudy-blue-sky-45P-Jib0OHA", "/api/parks/16/queue_times.json", 130, 300);
+const disneyCaliforniaAdventure = new Park("Disneyland California Adventure", "USA", "https://source.unsplash.com/a-ferris-wheel-with-flowers-in-front-of-it-G_IW5FLKZiQ", "/api/parks/17/queue_times.json", 130, 300);
 
 
 
